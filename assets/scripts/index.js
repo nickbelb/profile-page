e1064b0f4d5c044237733e6ebb6b6fc9f769bd4a
Nicolas Beltran <nicolasfelipebeltran@gmail.com>
2023-11-11 00:00:33 -0600

    final touches

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,33 @@
-'use script'
+'use strict';
+
+function selectObject(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+
+const getInTouchBtn = selectObject('.get-in-touch');
+const aboutMeBtn = selectObject('.more');
+const aboutMeSection = selectObject('#about-me');
+const contactMe=selectObject("#contact-me");
+
+
+function goToAboutMeSection() {
+    aboutMeSection.scrollIntoView({
+        behavior: 'smooth',
+        block: "end"
+    });
+}
+function goToContactMeSection(){
+    contactMe.scrollIntoView({
+        behavior: 'smooth',
+        block: "start"
+    });
+} 
+
+onEvent('click',getInTouchBtn,goToContactMeSection);
+onEvent('click',aboutMeBtn,goToAboutMeSection);
+
